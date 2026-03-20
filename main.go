072d39581df59e6928fd65903d319eb33e73886f
dora-explora <sarrafdara@gmail.com>
2026-03-20 00:32:27 -0700

    oops forgot the ssh functionality

--- main.go
+++ main.go
@@ -1,17 +1,60 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"errors"
+	"net"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	tea "charm.land/bubbletea/v2"
+	"charm.land/log/v2"
+	"charm.land/wish/v2"
+	"charm.land/wish/v2/bubbletea"
+	"charm.land/wish/v2/logging"
+	"github.com/charmbracelet/ssh"
 )
 
-func notmain() {
-    program := tea.NewProgram(initialModel())
-    _, err := program.Run();
-    if err != nil {
-        fmt.Printf("An error occured: %v", err)
-        os.Exit(1)
-    }
+const (
+	host = "0.0.0.0"
+	port = "1234"
+)
+
+func main() {
+	s, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort(host, port)),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithMiddleware(
+			bubbletea.Middleware(teaHandler),
+			logging.Middleware(),
+		),
+	)
+	if err != nil {
+		log.Error("Could not start server", "error", err)
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Info("Starting SSH server", "host", host, "port", port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("Could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("Could not stop server", "error", err)
+	}
+}
+
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	m := initialModel()
+	return m, []tea.ProgramOption{}
 }
